027b0c49ac1b2a91534624940ba103a31e47a0ec
Eracolo <info@eracolo.com>
2026-06-16 15:17:01 +0200

    Remove refetch interval

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
-    queries: { refetchOnWindowFocus: true, refetchInterval: 60_000 },
+    queries: { refetchOnWindowFocus: true },
   },
 });
 
